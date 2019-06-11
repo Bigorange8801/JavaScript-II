4e5d6d1f6f3be0e363587a13a87466edc54641a9
John <cjd8801@gmail.com>
2019-06-11 17:10:19 -0500

    closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function firstFunc() {
+  var name = "Bill";
+  function displayName(){
+    alert (name);
+  }
+  return displayName;
+}
+firstFunc();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
